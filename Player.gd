2d26f667a486d03e7ccfd82b62b2454c58c72a66
Jake Parkhouse <jparkhouse1511@gmail.com>
2024-07-26 12:52:43 +0100

    initial commit

---
+++ Player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@export var speed = 400
+var screen_size
+var _target_angle
+@onready var tween = get_tree().create_tween()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta) -> void:
+	var input = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+	
+	var velocity
+	
+	if input.length_squared() > 0:
+		_target_angle = input.angle()
+
+		# Smoothly interpolate rotation using tween
+		var rotation_time = 0.2
+		tween.tween_property(self, "rotation", _target_angle, rotation_time)
+		
+		velocity = input.normalized() * speed
+		$AnimatedSprite2D.play("default")
+	else:
+		$AnimatedSprite2D.stop()
+		velocity = Vector2.ZERO
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
